74e5596168ff85358d4c35545dc3ee6304cee375
Joyce <joycelamsaungan@gmail.com>
2018-07-07 22:24:22 -0700

    adding django tutorial files

---
+++ polls/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import url
+
+from . import views
+
+
+app_name = "polls"
+
+urlpatterns = [
+	# /polls/
+	url(r'^$', views.IndexView.as_view(), name="index"),
+
+	# /polls/5/
+	url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name="detail"),
+
+	# /polls/5/results/
+	url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name="results"),
+
+	# /polls/5/vote/
+	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name="vote"),
+
+
+]
